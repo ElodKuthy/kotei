617f6647cd7aa8798280f1e06b65bcd1f11d14d5
ElodKuthy <kuthyelod@gmail.com>
2016-04-01 13:19:40 +0200

    Add administration page

--- client/administration/administration.js
+++ client/administration/administration.js
@@ -3,7 +3,24 @@ angular.module('kotei')
 
         $stateProvider
             .state('administration', {
-                abstract: true,
-                url: '/administration'
-            })
+                url: '/administration',
+                views: {
+                    'navbar@': {
+                        templateUrl: 'navbar/navbar.html',
+                        controller: 'NavbarController as navbar'
+                    },
+                    'content@': {
+                        templateUrl: 'administration/administration.html',
+                        controller: 'AdministrationController as administration'
+                    }
+                },
+                roles: ['coach', 'admin']
+        })
+    })
+    
+    .controller('AdministrationController', function (userInfoService) {
+
+        const userInfo = userInfoService.getUserInfo()
+
+        this.isAdmin = userInfo.isAdmin
     })
