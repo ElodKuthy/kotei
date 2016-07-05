51ac88f9fb47311395c70d602ae8a80836f34598
ElodKuthy <kuthyelod@gmail.com>
2016-07-05 17:03:42 +0200

    Add additonal gyms' email templates

---
+++ api/mail/zuglo.js
@@ -0,0 +1,155 @@
+const moment = require('moment')
+
+const from = '360Gym Zugló <no-reply@kotei.hu>'
+
+const sendResetPasswordToken = {
+    subject: '360Gym Zugló - Elfelejtett jelszó',
+    html: (user, token) =>
+        `<html>
+        <head>
+            <style type="text/css">
+                div {
+                    margin-bottom: 10px;
+                }
+            </style>
+        </head>
+        <body>
+        <div>Kedves ${user.fullName}!</div>
+
+        <div>Azt jelezted felénk, hogy elfelejtetted a jelenlegi jelszavadat.</div>
+
+        <div>
+            A következő linken tudsz új jelszót megadni:<br/>
+            <a href="https://zuglo.kotei.hu/reset-password/${token}">https://zuglo.kotei.hu/reset-password/${token}</a><br/>
+        </div>
+        
+        <div>Amennyiben nem te kezdeményezted a jelszó változtatást, kérlek feltétlenül jelezd ezt <a href="mailto:support@kotei.hu">support@kotei.hu</a> email címen.</div>
+
+
+        <div>
+            Üdvözlettel,<br/>
+            A 360Gym Zugló Csapata
+        </div>
+
+        <div>
+            P.S.: Ez egy automatikus üzenet, kérjük erre az emailre ne válaszolj.
+        </div>
+        </body>
+        <html>`
+}
+
+const sendRegistration = {
+    subject: '360Gym Zugló - Üdvözlünk',
+    html: (user, token) =>
+        `<html>
+        <head>
+            <style type="text/css">
+                div {
+                    margin-bottom: 10px;
+                }
+            </style>
+        </head>
+        <body>
+        <div>Kedves ${user.fullName}!</div>
+
+        <div>Nagyon örülünk, hogy a 360Gym Zugló Terem tagjainak körében üdvözölhetünk!</div>
+
+        <div>
+            A 360Gym Zugló Terem Kotei használatához aktiválnod kell a felhasználódat és megadnod egy jelszót, amit a következő linken tudsz megtenni:<br/>
+            <a href="https://zuglo.kotei.hu/reset-password/${token}">https://zuglo.kotei.hu/reset-password/${token}</a><br/>
+        </div>
+
+        <div>
+            A 360Gym Zugló Terem Koteit az alábbi webcímen tudod elérni:<br/>
+            <a href="https://zuglo.kotei.hu">https://zuglo.kotei.hu</a><br/>
+            Itt megnézheted a terem órarendjét, illetve hogy mely órákra jelentkezél, és még sok minden mást is!
+        </div>
+        
+        <div>Ha további kérdéseid lennének, fordulj bizalommal edződhöz személyesen, vagy írj nekünk a <a href="mailto:support@kotei.hu">support@kotei.hu</a> email címre.</div>
+
+        <div>
+            Üdvözlettel,<br/>
+            A 360Gym Zugló Csapata
+        </div>
+
+        <div>
+            P.S.: Ez egy automatikus üzenet, kérjük erre az emailre ne válaszolj.
+        </div>
+        </body>
+        <html>`
+
+}
+
+const sendSubscriptionAlmostDepletedNotification = {
+    subject: '360Gym Zugló - Emlékeztető bérlet lejáratáról',
+    html: (subscription) =>
+        `<html>
+        <head>
+            <style type="text/css">
+                div {
+                    margin-bottom: 10px;
+                }
+            </style>
+        </head>
+        <body>
+        <div>Kedves ${subscription.Client.fullName}!</div>
+
+        <div>Csak szerettünk volna szólni, hogy a bérleted hamarosan le fog járni. Ezt a bérletedet ${moment(subscription.from).format('YYYY. MM. DD')}-n vásároltad, ${moment(subscription.to).format('YYYY. MM. DD')}-ig érvényes, és összesen ${subscription.all} edzésalkalomra szól. Már csak ${subscription.remaining} alkalmad van hátra.</div>
+        
+        <div>Új bérletet akkor kell venned, ha az összes alkalmadat lejártad az aktuális bérleteden, vagy annak érvényességi ideje lejárt.</div>
+        
+        <div>Ha további kérdéseid lennének, fordulj bizalommal edződhöz személyesen, vagy írj nekünk a <a href="mailto:support@kotei.hu">support@kotei.hu</a> email címre.</div>
+
+        <div>
+            Üdvözlettel,<br/>
+            ${subscription.Coach.fullName}
+        </div>
+
+        <div>
+            P.S.: Ez egy automatikus üzenet, kérjük erre az emailre ne válaszolj.
+        </div>
+        </body>
+        <html>`
+}
+
+const sendNewSubscriptionNotification = {
+    subject: '360Gym Zugló - Emlékeztető bérlet vásárlásról',
+    html: (subscription) =>
+        `<html>
+        <head>
+            <style type="text/css">
+                div {
+                    margin-bottom: 10px;
+                }
+            </style>
+        </head>
+        <body>
+        <div>Kedves ${subscription.Client.fullName}!</div>
+
+        <div>Ez csak egy emlékeztető, hogy új bérletet vásároltál. Ezt a bérletedet ${moment(subscription.from).format('YYYY. MM. DD')}-n vásároltad, 
+        ${moment(subscription.to).format('YYYY. MM. DD')}-ig érvényes, és összesen ${subscription.all} edzésalkalomra szól.</div>
+        
+        <div>Új bérletet akkor kell venned, ha az összes alkalmadat lejártad az aktuális bérleteden, vagy annak érvényességi ideje lejárt.</div>
+        
+        <div>Ha további kérdéseid lennének, fordulj bizalommal edződhöz személyesen, vagy írj nekünk a <a href="mailto:support@kotei.hu">support@kotei.hu</a> email címre.</div>
+
+        <div>
+            Üdvözlettel,<br/>
+            ${subscription.Coach.fullName}
+        </div>
+
+        <div>
+            P.S.: Ez egy automatikus üzenet, kérjük erre az emailre ne válaszolj.
+        </div>
+        </body>
+        <html>`
+}
+
+
+module.exports = {
+    from: from,
+    sendResetPasswordToken: sendResetPasswordToken,
+    sendRegistration: sendRegistration,
+    sendSubscriptionAlmostDepletedNotification: sendSubscriptionAlmostDepletedNotification, 
+    sendNewSubscriptionNotification: sendNewSubscriptionNotification
+}
