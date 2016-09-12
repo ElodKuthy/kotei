70ee7b3ff47bfdfe02d76fe03e8d1add3d74cd74
ElodKuthy <kuthyelod@gmail.com>
2016-09-12 22:16:01 +0200

    Add nice error message for Safari private mode

--- client/service/login-service.js
+++ client/service/login-service.js
@@ -1,23 +1,21 @@
 angular.module('kotei')
     .service('loginService', ($http, $q) => {
         return {
-            login: (credential, password) => {
-                return $http({
+            login: (credential, password) => $http({
                     method: 'POST',
                     url: '/api/login',
                     skipAuthorization: true,
                     data: {
                         userName: credential,
                         password: password
+                    }   
+                }).then(result => {
+                    try {
+                        return localStorage.setItem('jwt', result.data.jwt)
+                    } catch (err) {
+                        return $q.reject(err.code === 22 ? 'A Safari böngésző privát módjában nem lehet bejelentkezni. Használj normál módot, vagy egy másik fajta böngészőt!' : err)
                     }
-                }).then((result) => {
-                    return localStorage.setItem('jwt', result.data.jwt)
-                }, (error) => {
-                    return $q.reject(error.data.Error)
-                })
-            },
-            logout: () => {
-                localStorage.removeItem('jwt')
-            }
+                }, error => $q.reject(error.data.Error)),
+            logout: () => localStorage.removeItem('jwt')
         }
     })
