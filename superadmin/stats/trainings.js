57fb2a8f1d7b774026d0a39e0eab4899ec747bf5
ElodKuthy <kuthyelod@gmail.com>
2016-10-19 22:33:11 +0200

    Add csv export and subset checkbox to trainings view

--- superadmin/stats/trainings.js
+++ superadmin/stats/trainings.js
@@ -18,17 +18,37 @@ angular.module('superadmin')
                 roles: ['admin']
         })
     })
-    .controller('TrainingsController', function ($scope, $state, $moment, infoService) {
+    .controller('TrainingsController', function ($scope, $state, $filter, $moment, infoService) {
 
         this.date = $moment().endOf('month').toDate()
         this.minDate = $moment('2016-01-01').toDate()
         this.maxDate = $moment().endOf('month').toDate()
+        this.onlyCore = true
+        const coreGyms = ['retro', 'omszk', 'zuglo']
 
         this.dateChanged = () => {
             delete this.trainingsStats
-            infoService.getTrainingsStats(this.date).then(trainingsStats => this.trainingsStats = trainingsStats)
+            infoService.getTrainingsStats(this.date)                
+                .then(trainingsStats => this.trainingsStats = trainingsStats
+                .filter(stat => !this.onlyCore || coreGyms.indexOf(stat.gym) > -1))
         }
 
         $scope.$watch(() => this.date, this.dateChanged)
 
+        this.exportTrainings = () => {            
+            return this.trainingsStats.reduce((acc, curr) => {
+                return acc.concat(curr.trainings.map(training => {
+                    return {
+                        gym: curr.gym,
+                        name: training.name,
+                        date: $filter('date')(training.from, 'EEEE HH:mm'),
+                        coach: training.coach,
+                        location: training.location,
+                        utilization: training.utilization,
+                        max: training.max,
+                        percentage: Math.round(training.utilization / training.max * 100) + '%'
+                    }
+                }))
+            }, [])
+        }
 })
